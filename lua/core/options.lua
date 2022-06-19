1e2a2cdca7efb5687ed88e6e490cd99737d583a7
elairavi <elairavi@users.noreply.github.com>
2022-06-19 02:41:52 +0100

    options: fix typos and add setting to disable folding text

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -14,20 +14,20 @@ set.updatetime = 0                           -- Faster completion
 set.encoding="utf-8"                         -- The encoding displayed
 set.fileencoding="utf-8"                     -- The encoding written to file
 set.smartindent = true                       -- Makes indenting smart
-set.iskeyword:append("-")                    -- treat dash separated words as a word text object"
+set.iskeyword:append("-")                    -- Treat dash separated words as a word text object"
 set.clipboard = "unnamedplus"                -- Copy paste between vim and everything else
 set.smarttab = true                          -- Makes tabbing smarter will realize you have 2 vs 4
 set.expandtab = true                         -- Converts tabs to spaces
 set.autoindent = true                        -- Good auto indent
 set.autochdir = true                         -- Your working directory will always be the same as your working directory
-set.incsearch = true                         -- sets incremental search
+set.incsearch = true                         -- Sets incremental search
 set.shell = "/bin/zsh"                       -- Set your shell to bash or zsh
 set.shortmess:append "sI"                    -- Disable nvim intro
-vim.cmd [[set nobackup]]                     -- creates a backup file
-vim.cmd [[set nowritebackup]]                -- creates a backup file i guess
+vim.cmd [[set nobackup]]                     -- Creates a backup file
+vim.cmd [[set nowritebackup]]                -- Creates a backup file i guess
 vim.cmd [[set formatoptions-=cro]]           -- Stop newline continution of comments
-vim.cmd [[set complete+=kspell]]             -- auto complete with spellcheck
-vim.cmd [[set completeopt=menuone,longest]]  -- auto complete menu (It's pretty great)
+vim.cmd [[set complete+=kspell]]             -- Auto complete with spellcheck
+vim.cmd [[set completeopt=menuone,longest]]  -- Auto complete menu (It's pretty great)
 vim.cmd [[set nocompatible]]                 -- Disable compatibility to old-time vi
 -- set.mouse = 'a'                              -- Enable mouse support
 
@@ -41,11 +41,11 @@ set.splitright = true                        -- Vertical splits will automatical
 set.conceallevel = 0                         -- So that I can see `` in markdown files
 set.tabstop = 2                              -- Insert 2 spaces for a tab
 set.number = true                            -- Line numbers
-set.background = "dark"                      -- tell vim what the background color looks like
+set.background = "dark"                      -- Tell vim what the background color looks like
 set.virtualedit = "onemore"                  -- With This option you can move the cursor one character over the end
-set.ignorecase = true                        -- ignores case when searching
-set.smartcase = true                         -- turns on case sensitive search when letters are capitalized
-set.termguicolors = true                     -- set term gui colors (most terminals support this)
+set.ignorecase = true                        -- Ignores case when searching
+set.smartcase = true                         -- Turns on case sensitive search when letters are capitalized
+set.termguicolors = true                     -- Set term gui colors (most terminals support this)
 set.laststatus=3                             -- Always display the status line
 set.title = true                             -- Show current txt that you editing
 set.relativenumber = false                   -- Vim’s absolute, relative and hybrid line numbers
@@ -53,7 +53,8 @@ set.cursorline = true                        -- Enable highlighting of the curre
 set.shiftwidth = 2                           -- Change the number of space characters inserted for indentation
 set.showtabline = 1                          -- Always show tabs
 set.cmdheight = 1                            -- More space for displaying messages
-set.numberwidth = 5                          -- set number column width to 2 {default 4}
+set.numberwidth = 5                          -- Set number column width to 2 {default 4}
+set.foldenable = false                       -- Disable folding text
 vim.cmd [[setlocal conceallevel=2]]          -- Syntax conceal for markdown
 vim.cmd [[set noshowmode]]                   -- We don't need to see things like -- INSERT -- anymore
 vim.cmd [[syntax enable]]                    -- Enables syntax highlighing
