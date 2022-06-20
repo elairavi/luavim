9653dda6e99ad6625de7f9b0b173117e22f75399
elairavi <elairavi@users.noreply.github.com>
2022-06-20 03:53:12 +0100

    telescope.lua: add telescope config

---
+++ lua/configs/telescope.lua
@@ -0,0 +1,32 @@
+
+--[[--------------------------------------]--
+--         telescope - fuzzy finder        --
+--        Author: ChristianChiarulli       --
+--             License: GPLv3              --
+--[[-------------------------------------]]--
+
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+   return
+end
+
+local actions = require "telescope.actions"
+
+telescope.setup {
+  defaults = {
+
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+
+    mappings = {
+      i = {
+        ["<Down>"] = actions.cycle_history_next,
+        ["<Up>"] = actions.cycle_history_prev,
+        ["<A-j>"] = actions.move_selection_next,
+        ["<A-k>"] = actions.move_selection_previous,
+      },
+    },
+  },
+}
