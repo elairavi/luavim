4c313aa0be6a2fb1776f834669cb54ccd224429c
elairavi <elairavi@users.noreply.github.com>
2022-06-23 20:45:31 +0100

    commentary: removed unneeded option

--- lua/configs/commentary.lua
+++ lua/configs/commentary.lua
@@ -11,6 +11,5 @@ require('nvim_comment').setup({
    comment_empty = true,                   -- should comment out empty or whitespace only lines
    comment_empty_trim_whitespace = true,   -- trim empty comment whitespace
    create_mappings = true,                 -- Should key mappings be created
-   operator_mapping = "<leader>g",         -- Visual/Operator mapping left hand side
    hook = nil                              -- Hook function to call before commenting takes place
 })
