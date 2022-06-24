7b24ed860be74d0a87b30c2a1030b70a07898872
elairavi <elairavi@users.noreply.github.com>
2022-06-24 12:02:13 +0100

    neoscroll: load neoscroll with a pcall and cleaned up the config

--- lua/configs/neoscroll.lua
+++ lua/configs/neoscroll.lua
@@ -5,30 +5,35 @@
 --             License: GPLv3               --
 --[[--------------------------------------]]--
 
--- Load neoscroll
-require('neoscroll').setup({
-    pre_hook = function(info) if info == "cursorline" then vim.wo.cursorline = false end end,
-    post_hook = function(info) if info == "cursorline" then vim.wo.cursorline = true end end
-})
+-- Load neoscroll with a protected call
+local status_ok, neoscroll = pcall(require, "neoscroll")
+if not status_ok then
+   return
+end
 
 -- Neoscroll options
-local t = {}
+neoscroll.setup {
+    pre_hook = function(info) if info == "cursorline" then vim.wo.cursorline = false end end,
+    post_hook = function(info) if info == "cursorline" then vim.wo.cursorline = true end end
+}
 
--- ['alt + u'] for scrolling up , ['alt + d'] for scrolling down
-t['<A-u>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '250'}}
-t['<A-d>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250'}}
+-- Custom mappings
+local ns = {}
 
--- ['alt + k'] for scrolling up , ['alt + j'] for scrolling down)
-t['<A-k>'] = {'scroll', {'-vim.wo.scroll', 'false', '150', [['sine']]}}
-t['<A-j>'] = {'scroll', { 'vim.wo.scroll', 'false', '150', [['sine']]}}
+-- [alt + u for scrolling up] , [alt + d for scrolling down]
+ns['<A-u>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '250'}}
+ns['<A-d>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250'}}
 
--- ['ctrl + b'] for scrolling up , ['ctrl + f'] for scrolling down)
-t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '250'}}
-t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250'}}
+-- [ctrl + b for scrolling up] , [ctrl + f for scrolling down]
+ns['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '250'}}
+ns['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250'}}
 
--- When no easing function is provided the default easing function (in this case "quadratic") will be used
-t['zz'] = {'zz', {'200'}}
-t['zb'] = {'zb', {'200'}}
-t['zt'] = {'zt', {'200'}}
+-- [alt + k for scrolling up] , [alt + j for scrolling down]
+ns['<A-k>'] = {'scroll', {'-vim.wo.scroll', 'false', '150', [['sine']]}}
+ns['<A-j>'] = {'scroll', { 'vim.wo.scroll', 'false', '150', [['sine']]}}
 
-require('neoscroll.config').set_mappings(t)
+-- When no easing function is provided the default "quadratic" will be used
+ns['zz'] = {'zz', {'200'}}
+ns['zb'] = {'zb', {'200'}}
+
+require('neoscroll.config').set_mappings(ns)
