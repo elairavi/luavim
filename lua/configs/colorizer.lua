e388401244eca0e0b1fc4b8629c1ae62a0a677f6
elairavi <elairavi@users.noreply.github.com>
2022-06-24 06:28:10 +0100

    colorizer: removed auto command and used 'colorizer.setup' instead

--- lua/configs/colorizer.lua
+++ lua/configs/colorizer.lua
@@ -1,15 +1,24 @@
 
---[[-------------------------------------]]--
---      colorizer - colors highlighter     --
---             Author: elai                --
---            License: GPLv3               --
---[[-------------------------------------]]--
+--[[------------------------------------]]--
+--     colorizer - colors highlighter     --
+--    Author: ChristianChiarulli, elai    --
+--           License: GPLv3               --
+--[[------------------------------------]]--
 
 -- Load colorizer with a protected call
 local status_ok, colorizer = pcall(require, "colorizer")
 if not status_ok then
-    return
+   return
 end
 
--- Toggle colorizer on enter
-vim.cmd[[autocmd VimEnter * :ColorizerToggle]]
+colorizer.setup({ "*" }, {
+    RGB      = true;      -- #RGB hex codes
+	  RRGGBB   = true;      -- #RRGGBB hex codes
+	  RRGGBBAA = true;      -- #RRGGBBAA hex codes
+	  rgb_fn   = true;      -- CSS rgb() and rgba() functions
+	  hsl_fn   = true;      -- CSS hsl() and hsla() functions
+	  names    = false;     -- "Name" codes like Blue
+	  css      = false;     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	  css_fn   = false;     -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    mode = "background",  -- Set the display mode / Available modes: foreground, background, virtualtext
+})
