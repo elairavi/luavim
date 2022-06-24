e871e9bced5814bc6d77f69805586d7ea960ce6e
elairavi <elairavi@users.noreply.github.com>
2022-06-24 22:11:12 +0100

    commentary: load 'nvim_comment' with a protected call

--- lua/configs/commentary.lua
+++ lua/configs/commentary.lua
@@ -5,11 +5,17 @@
 --              License: GPLv3               --
 --[[---------------------------------------]]--
 
+-- Load nvim_comment with a protected call
+local status_ok, nvim_comment = pcall(require, "nvim_comment")
+if not status_ok then
+   return
+end
+
 -- Nvim comment options
-require('nvim_comment').setup({
+nvim_comment.setup {
+   comment_empty = true,                   -- Should comment out empty or whitespace only lines
    marker_padding = true,                  -- Linters prefer comment and line to have a space in between markers
-   comment_empty = true,                   -- should comment out empty or whitespace only lines
-   comment_empty_trim_whitespace = true,   -- trim empty comment whitespace
    create_mappings = true,                 -- Should key mappings be created
+   comment_empty_trim_whitespace = true,   -- Trim empty comment whitespace
    hook = nil                              -- Hook function to call before commenting takes place
-})
+}
