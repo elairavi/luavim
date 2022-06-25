8dcba89470056637ea8296f3150896af92b86354
elairavi <elairavi@users.noreply.github.com>
2022-06-25 06:31:34 +0100

    utils: changed config name and added blinking cursor

--- lua/utils/utils.lua
+++ lua/utils/utils.lua
@@ -14,6 +14,13 @@ autocmd BufWritePre *.[ch] %s/\%$/\r/e
 autocmd BufWritePre * cal cursor(currPos[1], currPos[2])
 ]], false)
 
+-- Blinking Cursor
+vim.cmd [[
+		:set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+		  \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
+		  \,sm:block-blinkwait175-blinkoff150-blinkon175
+]]
+
 -- Abbreviations
 vim.cmd([[
 cnoreabbrev W! w!
